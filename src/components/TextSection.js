81548f3acfeea469832826144b707b319cd8f272
uzair shamim <uzair.shamim15@gmail.com>
2022-02-10 21:43:49 +0500

    second commit

--- src/components/TextSection.js
+++ src/components/TextSection.js
@@ -1,6 +1,6 @@
 import "../styles/TextSection.css";
 import React, { useEffect } from 'react';
-import { gsap } from "gsap"; 
+import { gsap, Power3, Linear } from "gsap"; 
 // import { TweenLite } from "gsap";
 
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -33,14 +33,30 @@ function TextSection() {
 
     let tl3 = gsap.timeline();
     tl3.to("#scrollingText3", {
-      x: 600,
-      backgroundPosition: '600px 0',
+      x: 300,
+      backgroundPosition: '300px 0',
       scrollTrigger: {
         trigger: "body",
         start: 'top top',
         scrub: true,
       }
     })
+
+
+    let tl4 = gsap.timeline();
+    tl4.to(".words-img", {
+      rotation:"360", 
+      // ease:Linear.easeNone, 
+      // repeat:-1,
+      scrollTrigger: {
+        trigger: "body",
+        start: 'top top',
+        scrub: true,
+      }
+    })
+
+
+
 
     
   },[])
@@ -49,6 +65,10 @@ function TextSection() {
           <h2 id="scrollingText1">own your plug</h2>
           <h1 id="scrollingText2">own your plug</h1>
           <h2 id="scrollingText3">own your plug</h2>
+          <div className="words-smoke-sec">
+            <img src={process.env.PUBLIC_URL + "images/words.png"} className="img-fluid words-img" alt="words" />
+            <img src={process.env.PUBLIC_URL + "images/smoke-1.png"} className="img-fluid" alt="smoke" />
+          </div>
       </div>
     );
   }
